29e0f50a5628be2a6d6bd8499ae21492bb02f150
shashi-123jha <ranjanshashi854105@gmail.com>
2024-07-15 01:34:43 +0530

    taking contents from the github

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ prev.addEventListener('click', function(){
     document.querySelector('.slide').prepend(items[items.length - 1]) 
 })
 
+document.getElementById('btn-ripple').addEventListener('click', function(e) {
+    const ripple = document.createElement('span');
+    ripple.classList.add('ripples');
+    this.appendChild(ripple);
+
+    const rect = this.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    gsap.fromTo(ripple, {
+        left: x,
+        top: y,
+        height: 0,
+        width: 0,
+        opacity: 1,
+    }, {
+        height: size,
+        width: size,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+        onComplete: () => ripple.remove()
+    });
+});
