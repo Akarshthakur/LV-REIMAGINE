a974b86a5a66a7363241a9abb69068f85d8c701b
shashi-123jha <ranjanshashi854105@gmail.com>
2024-07-15 21:14:06 +0530

    page3 products section fixed

--- script.js
+++ script.js
@@ -62,3 +62,5 @@ mybutton.addEventListener("click", function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
+
+
